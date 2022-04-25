5f517a62c3c5d974fa844fd09c2a0453eecd505b
alterego88 <31219398+alterego88@users.noreply.github.com>
2022-04-25 15:46:44 +0400

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,27 +1,4 @@
-$(document).ready(function(){
 
-  $('.counter').each(function() {
-    var $this = $(this),
-        countTo = $this.attr('data-count');
-
-    $({ countNum: $this.text()}).animate({
-      countNum: countTo
-    },
-
-    {
-
-      duration: 3000,
-      easing:'linear',
-      step: function() {
-        $this.text(Math.floor(this.countNum));
-      },
-      complete: function() {
-        $this.text(this.countNum);
-      }
-    });
-  });
-
-});
 /*
     = = = = = YOU WALLET ADRESS = = = = =
 */
