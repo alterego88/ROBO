c6bf0237ae5f42e8a4b2df8988139c038e85ff07
alterego88 <31219398+alterego88@users.noreply.github.com>
2022-04-25 13:35:44 +0400

    Update settings.js

--- settings.js
+++ settings.js
@@ -16,12 +16,12 @@ const collectionInfo = {
         twitter: "https://twitter.com/username_here",
     },
     medias: {
-        preview: "preview.gif",
+        preview: "tg-gif.gif",
         favicon: "icon.png",
     },
     background: {
         type: "image", // Supported types: image, video, color (use image for gif)
-        image: "background.mp4", // Image for image type or video preview for video type (image befor video load)
+        image: "background.jpg", // Image for image type or video preview for video type (image befor video load)
         video: "background.mp4", // If you don't use video type, you can ignore this line
         color: "#4E4E6D", // If you don't use color type, you can ignore this line
     }
